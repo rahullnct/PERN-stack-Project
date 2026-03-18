e3bfe962c5976676dc9e05b7753ce32f7635b20e
rahullnct <rahulyadavkv01@gmail.com>
2026-03-18 11:28:35 +0530

    project task work is started

---
+++ mern-projects/src/All_Dialogue/Newtask.jsx
@@ -0,0 +1,9 @@
+
+function NewTask({setisopen}){
+    return(
+        <div>
+            <h1>New Task</h1>
+        </div>
+    )
+};
+export default NewTask;
--- mern-projects/src/Pages/ProjectDetail.jsx
+++ mern-projects/src/Pages/ProjectDetail.jsx
@@ -1,14 +1,43 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
-
+import { useNavigate, useParams } from "react-router-dom";
+import {ArrowLeft,Activity} from "lucide-react";
+import NewTask from "../All_Dialogue/Newtask";
 function ProjectDetail({project_id}){
     let {id }=useParams();
-    const currentWorkspace=useSelector((state)=> state.workspace.workspaces);
-    console.log(currentWorkspace);
+    const navigate=useNavigate();
+    const currentWorkspace=useSelector((state)=> state.workspace.currentWorkspace);
+    const[isOpen,setisopen]=useState(false);
+    // console.log(currentWorkspace);
+    let newProjects= currentWorkspace?.projects?.find((item)=> item.id === id);
+    // console.log("new_projects:",newProjects);
+     const ProjectDetailCards=[
+        {title:"Total Tasks",icon: Activity, card_number:1},
+        {title:" Completed",icon: Activity, card_number:2},
+        {title:"In Progress",icon: Activity , card_number:3},
+        {title:"Team Members",icon: Activity , card_number:4}
+    ]
     return(
-        <div>
-           <h1>ProjectDetail</h1> 
-           <h2>{id}</h2>
+        <div className="task_wrapper">
+           <div className="task_container">
+            <div className="task_upper_container">
+                <div className="heading_container">
+                    <ArrowLeft size={15} onClick={()=>navigate('/project')}/>
+                <h2 className="task_heading">{newProjects.name}</h2>
+                <div className="task_status_container">
+                    <span className="task_status">{newProjects?.status}</span>
+                </div>
+                </div>
+                <button className="new_task_btn" onClick={()=>setisopen(true)}>
+                    + New Task
+                </button>
+                {
+                    isOpen && 
+                    <NewTask setisopen={setisopen}/>
+                }
+            </div>
+              
+           </div>
         </div>
         
         
